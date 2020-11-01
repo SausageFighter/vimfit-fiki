07b6fd63261f0703d12a220b3c63ab3700141ce8
SausageFighter <mihaialexds@gmail.com>
2020-11-01 19:00:21 +0200

    conclusion section M2

--- src/app/milestone2/milestone2.component.ts
+++ src/app/milestone2/milestone2.component.ts
@@ -9,7 +9,32 @@ import { MatIconRegistry } from '@angular/material/icon';
 })
 export class Milestone2Component implements OnInit {
 
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) { }
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'analytics',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/m2-features/analytics.svg'));
+    iconRegistry.addSvgIcon(
+      'bullhorn',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/m2-features/bullhorn.svg'));
+    iconRegistry.addSvgIcon(
+      'calendar',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/m2-features/calendar.svg'));
+    iconRegistry.addSvgIcon(
+      'dubbing',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/m2-features/dubbing.svg'));
+    iconRegistry.addSvgIcon(
+      'map',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/m2-features/map.svg'));
+    iconRegistry.addSvgIcon(
+      'training',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/m2-features/training.svg'));
+    iconRegistry.addSvgIcon(
+      'video',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/m2-features/video.svg'));
+    iconRegistry.addSvgIcon(
+      'yoga',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/m2-features/yoga.svg'));
+  }
 
   ngOnInit(): void {
   }
