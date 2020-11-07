e70ddd2bb35919230178eb6d3574c7fce7a31416
Daniel <daniel.berbece97@gmail.com>
2020-11-08 00:34:43 +0200

    Add features section

--- src/app/milestone3/milestone3.component.ts
+++ src/app/milestone3/milestone3.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'milestone3',
@@ -7,7 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Milestone3Component implements OnInit {
 
-  constructor() { }
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'training-red',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/training-red.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'chat',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/chat.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'resume',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/resume.svg')
+    );
+  }
 
   ngOnInit(): void {
   }
