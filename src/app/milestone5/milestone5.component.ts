9b3f2b8fe3a25fb0bc711a1e5a5a5147e6b85697
SausageFighter <mihaialexds@gmail.com>
2020-12-07 05:30:26 +0200

    M5 + live update

--- src/app/milestone5/milestone5.component.ts
+++ src/app/milestone5/milestone5.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./milestone5.component.scss']
 })
 export class Milestone5Component implements OnInit {
+  private readonly FOLDER_ANALYTICS = 'assets/analytics';
+  private readonly FOLDER_MARKETING = 'assets/marketing';
+
+  imagesAnalytics = ['traffic.png', 'interraction.png', 'devices.png', 'technologies.png'].map(name => `${this.FOLDER_ANALYTICS}/${name}`);
+  imagesMarketing = ['1.jpeg', '2.jpeg', '3.jpeg', '4.jpeg', '5.jpeg', '6.jpeg', '7.jpeg', '8.jpeg', '9.jpeg', '10.jpeg'].map(name => `${this.FOLDER_MARKETING}/${name}`);
 
   constructor() { }
 
